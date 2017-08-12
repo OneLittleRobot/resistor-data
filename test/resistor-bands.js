54a0770dfdebb7395dd91bd9f575566c0bd27ef0
Kevin Thomas <kevin@onelittlerobot.co.uk>
2017-08-12 14:25:57 +0100

    updated tests to catch errors and some edge cases

--- test/resistor-bands.js
+++ test/resistor-bands.js
@@ -1,69 +1,146 @@
 'use strict';
 var chai = require('chai'),
     assert = chai.assert,
-    expect = chai.expect,
-    resistorBands = require('../lib/resistor-bands');
-var resource;
-describe('Resistor Bands to Resistor Notation', function() {
-    it('2k4 - 5 bands', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'brown']), ['2k4', 1]);
+    resistorBands = require('../source/resistor-bands');
+
+describe('Error Resistor Bands to Resistor Notation', function () {
+
+    it('asserts not an array throws an error', function () {
+        assert.throws(function () {
+            resistorBands.bandsToNotation('banjo');
+        }, TypeError, 'Expected an array');
     });
-    it('33k - 5 bands', function() {
-        assert.deepEqual(resistorBands.decode(['orange', 'orange', 'black', 'red', 'brown']), ['33k', 1]);
+
+    it('asserts no bands throws an error', function () {
+        assert.throws(function () {
+            resistorBands.bandsToNotation([]);
+        }, Error, 'A resistor should have 4 or 5 bands');
     });
-    it('2k4 - 4 bands', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'red', 'brown']), ['2k4', 1]);
+
+    it('assert not enough bands throws an error', function () {
+        assert.throws(function () {
+            resistorBands.bandsToNotation(['red', 'yellow', 'black']);
+        }, Error, 'A resistor should have 4 or 5 bands');
     });
-    it('33k - 4 bands', function() {
-        assert.deepEqual(resistorBands.decode(['orange', 'orange', 'orange', 'brown']), ['33k', 1]);
+
+    it('asserts too many bands throws an error', function () {
+        assert.throws(function () {
+            resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'brown', 'brown']);
+        }, Error, 'A resistor should have 4 or 5 bands');
     });
+
+    it('asserts invalid values throws an error', function () {
+        assert.throws(function () {
+            resistorBands.bandsToNotation(['pink', 'banjo', 'ham sandwich', 'monkey', 'woo']);
+        }, Error);
+    });
+
 });
-describe('Resistor Notation to Resistor Bands', function() {
-    it('2k4 - 5 bands', function() {
-        assert.deepEqual(resistorBands.encode(['2k4', 0.5], 5), ['red', 'yellow', 'black', 'brown', 'green']);
+
+describe('Resistor Bands to Resistor Notation', function () {
+
+    it('2k4 - 5 bands', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'brown']), ['2k4', 1]);
     });
-    it('1  - 5 bands', function() {
-        assert.deepEqual(resistorBands.encode(['1', 0.5], 5), ['brown', 'black', 'black', 'silver', 'green']);
+
+    it('33k - 5 bands', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['orange', 'orange', 'black', 'red', 'brown']), ['33k', 1]);
     });
-    it('3M  - 5 bands', function() {
-        assert.deepEqual(resistorBands.encode(['3M', 0.5], 5), ['orange', 'black', 'black', 'yellow', 'green']);
+
+    it('2k4 - 4 bands', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'red', 'brown']), ['2k4', 1]);
     });
-    it('2k4 - 4 bands', function() {
-        assert.deepEqual(resistorBands.encode(['2k4', 0.5], 4), ['red', 'yellow', 'red', 'green']);
+
+    it('33k - 4 bands', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['orange', 'orange', 'orange', 'brown']), ['33k', 1]);
     });
-    it('1 - 4 bands', function() {
-        assert.deepEqual(resistorBands.encode(['1', 0.5], 4), ['brown', 'black', 'gold', 'green']);
-    });
-    it('3M - 4 bands', function() {
-        assert.deepEqual(resistorBands.encode(['3M', 0.5], 4), ['orange', 'black', 'green', 'green']);
-    });
+
 });
-describe('Resistor Bands to Resistor Notation - Tolerance', function() {
-    it('1%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'brown']), ['2k4', 1]);
+
+describe('Error Resistor Notation to Resistor Bands', function () {
+
+    it('asserts not an array throws an error', function () {
+        assert.throws(function () {
+            resistorBands.notationToBands('not an array', 5);
+        }, TypeError, 'Expected an array');
     });
-    it('2%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'red']), ['2k4', 2]);
+
+});
+
+
+describe('Resistor Notation to Resistor Bands', function () {
+
+    it('2k4 - 5 bands', function () {
+        assert.deepEqual(resistorBands.notationToBands(['2k4', 0.5], 5), ['red', 'yellow', 'black', 'brown', 'green']);
     });
-    it('0.5%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'green']), ['2k4', 0.5]);
+
+    it('1  - 5 bands', function () {
+        assert.deepEqual(resistorBands.notationToBands(['1', 0.5], 5), ['brown', 'black', 'black', 'silver', 'green']);
     });
-    it('0.25%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'blue']), ['2k4', 0.25]);
+
+    it('3M  - 5 bands', function () {
+        assert.deepEqual(resistorBands.notationToBands(['3M', 0.5], 5), ['orange', 'black', 'black', 'yellow', 'green']);
     });
-    it('0.1%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'violet']), ['2k4', 0.1]);
+
+    it('2k4 - 4 bands', function () {
+        assert.deepEqual(resistorBands.notationToBands(['2k4', 0.5], 4), ['red', 'yellow', 'red', 'green']);
     });
-    it('0.05%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'grey']), ['2k4', 0.05]);
+
+    it('1 - 4 bands', function () {
+        assert.deepEqual(resistorBands.notationToBands(['1', 0.5], 4), ['brown', 'black', 'gold', 'green']);
     });
-    it('5%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'gold']), ['2k4', 5]);
+
+    it('3M - 4 bands', function () {
+        assert.deepEqual(resistorBands.notationToBands(['3M', 0.5], 4), ['orange', 'black', 'green', 'green']);
     });
-    it('10%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'silver']), ['2k4', 10]);
+
+    it('assert defaults to 5 bands', function () {
+        assert.deepEqual(resistorBands.notationToBands(['3M', 0.5]), ['orange', 'black', 'black', 'yellow', 'green']);
     });
-    it('20%', function() {
-        assert.deepEqual(resistorBands.decode(['red', 'yellow', 'black', 'brown', 'none']), ['2k4', 20]);
+
+    it('assert defaults to 1% tolerance', function () {
+        assert.deepEqual(resistorBands.notationToBands(['3M'], 5), ['orange', 'black', 'black', 'yellow', 'brown']);
     });
+
+});
+
+describe('Resistor Bands to Resistor Notation - Tolerance', function () {
+
+    it('1%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'brown']), ['2k4', 1]);
+    });
+
+    it('2%', function () {
+
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'red']), ['2k4', 2]);
+    });
+
+    it('0.5%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'green']), ['2k4', 0.5]);
+    });
+
+    it('0.25%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'blue']), ['2k4', 0.25]);
+    });
+
+    it('0.1%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'violet']), ['2k4', 0.1]);
+    });
+
+    it('0.05%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'grey']), ['2k4', 0.05]);
+    });
+
+    it('5%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'gold']), ['2k4', 5]);
+    });
+
+    it('10%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'silver']), ['2k4', 10]);
+    });
+
+    it('20%', function () {
+        assert.deepEqual(resistorBands.bandsToNotation(['red', 'yellow', 'black', 'brown', 'none']), ['2k4', 20]);
+    });
+
 });
